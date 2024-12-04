a6a1e128f7880799290fe0aedd7412dae4dc6fd1
odie <odie.gamedev@gmail.com>
2024-12-04 12:29:01 -0500

    Update StartMenu.cs

--- StartMenu.cs
+++ StartMenu.cs
@@ -5,16 +5,29 @@ using UnityEngine.SceneManagement;
 public class StartMenu : Monobehavior
 {
     public gameObject pauseMenu;
+
+    private Void Update()
+    {
+        if (GameManager.Instance.CurrentState == GameManager.GameState.Playing)
+        {
+            {
+                Pause();
+            }
+            else if (GameManager.Instance.CurrentState == GameManager.GameState.Paused)
+            {
+                UnPause();
+            }
+        }
+    }
     
-private void Pause()
-    if(Input.GetKeyDown(KeyCode.Escape))
+    private void Pause()
     {
         GameManager.Instance.SetGameState(GameState.Paused)
         Time.timeScale = 0f; //pause time
     }
-private void UnPause()
+    private void UnPause()
     {
         GameManager.Instance.SetGameState(GameState.Playing)
-        Time.timescale = 0f; // resume time
+        Time.timescale = 1f; // resume time
     }
 }
