4c101a33d87e640b88570e5d9699565940d4b5bf
odie <odie.gamedev@gmail.com>
2024-12-04 11:39:14 -0500

    Update StartMenu.cs

--- StartMenu.cs
+++ StartMenu.cs
@@ -6,9 +6,15 @@ public class StartMenu : Monobehavior
 {
     public gameObject pauseMenu;
     
-
+private void Pause()
     if(Input.GetKeyDown(KeyCode.Escape))
     {
         GameManager.Instance.SetGameState(GameState.Paused)
+        Time.timeScale = 0f; //pause time
+    }
+private void UnPause()
+    {
+        GameManager.Instance.SetGameState(GameState.Playing)
+        Time.timescale = 0f; // resume time
     }
 }
