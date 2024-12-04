0ecc166a1d6145ef055f77e892e94b1c456ab086
odie <odie.gamedev@gmail.com>
2024-12-04 11:28:16 -0500

    Create StartMenu.cs

---
+++ StartMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartMenu : Monobehavior
+{
+    public gameObject pauseMenu;
+    
+
+    if(Input.GetKeyDown(KeyCode.Escape))
+    {
+        
+
+}
